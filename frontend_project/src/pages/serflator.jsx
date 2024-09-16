5dfe88b2fc269ff017823b5d2b69a607966d12f8
DrAzrael <u8_aleszl_lbn@technischools.com>
2024-09-16 15:34:10 +0200

    added 404 handling and base page for smerflator

---
+++ frontend_project/src/pages/serflator.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+export default function Smerflator() {
+// const [sex, setSex] = useState("Sex") /*tak się tworzy zmienne zazwyczaj*/
+
+return (
+    <>
+    <p>to wszystko wina tych smerfowanych żydów</p>
+    </>
+)
+}
--- frontend_project/src/router/router.jsx
+++ frontend_project/src/router/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import {Example_main_function as Example} from "../pages/example"; 
 import Lander_func from "../pages/lander"; 
+import Page_not_found from "../pages/pageNotFound"; 
+import Smerflator from "../pages/serflator";
 
 import Example_main_function from "../pages/example"; 
 
@@ -10,10 +12,17 @@ const router = createBrowserRouter([
     element: <Example_main_function />, /*tu dajesz funkcje którą zimportowałeś*/
   },
   {
-    path: "/", /*tu jest ścieżka na jakiej ma się wyświtlać w przeglądarce */ 
+    path: "/lander_page", /*tu jest ścieżka na jakiej ma się wyświtlać w przeglądarce */ 
     element: <Lander_func />, /*tu dajesz funkcje którą zimportowałeś*/
   },
-  /*jak chesz dodać nową stronę to kopiujesz to powyrzej i podmieniasz ścieżkę i funkcję */
+  {
+    path: "/smerf", /*tu jest ścieżka na jakiej ma się wyświtlać w przeglądarce */ 
+    element: <Smerflator />, /*tu dajesz funkcje którą zimportowałeś*/
+  },
+  {
+    path: "*", /*tu jest ścieżka na jakiej ma się wyświtlać w przeglądarce */ 
+    element: <Page_not_found />, /*tu dajesz funkcje którą zimportowałeś*/
+  },
   
 ]);
 
